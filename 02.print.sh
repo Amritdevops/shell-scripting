4b2f661a50006906d2dcb2831eb53fc9f8ef9d82
amrit <paulamrit23878@gmail.com>
2022-01-21 18:19:45 -0500

    print color script

--- 02.print.sh
+++ 02.print.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 echo hello world
+
+# color lines: used to highlight the message
+
+# syntax : echo-e\e[COLORmMESSAGE\[0m
+
+# -e to enable sequence \e
+
+# \e to enable color
+
+# [colorm is color number
+
+#\e[0m to disable the color
+
+echo -e\e[31AMRIT\e[32PAUL\e[33SINGH\0m
