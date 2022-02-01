c8d75c231486f5ad47aa7c6b01f164989942ad78
amrit <paulamrit23878@gmail.com>
2022-01-31 20:18:40 -0500

    erased

--- roboshop_project/components/frontend.sh
+++ roboshop_project/components/frontend.sh
@@ -22,29 +22,7 @@
 #
 #```
 ## systemctl restart nginx
-source components/common.sh
 
-echo installing nginx
-yum install nginx -y &>>$LOG_FILE
-echo "download front end"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
-
-echo   clean old content
-rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
-
-echo extract frontend content
-cd /tmp
-unzip -o frontend.zip &>>$LOG_FILE
-
-echo copy content to nginx location
-cp -r frontend-main/static/* /usr/share/nginx/html/ &>>$LOG_FILE
-
-echo copy nginx roboshop config
-cp frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
-
-echo start nginx service   
-systemctl enable nginx &>>$LOG_FILE
-systemctl start nginx &>>$LOG_FILE
 
 
 
