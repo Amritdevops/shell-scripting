040d0b0085235eb68f886a631561dce77ec39b33
amrit <paulamrit23878@gmail.com>
2022-02-09 18:19:13 -0500

    Updating shell-scripting

--- roboshop_project/components/frontend.sh
+++ roboshop_project/components/frontend.sh
@@ -36,24 +36,59 @@ fi
 
 echo "download frontend content"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
+if [ $? -eq 0 ]; then
+  echo -e "\e[1;32m success\e[0m"
+else
+  echo -e "\e[1;33m failed\e[0m"
+  exit
+fi
 
 echo "cleaning old content"
 rm -rf /usr/share/nginx/html &>>$LOG_FILE
+if [ $? -eq 0 ]; then
+  echo -e "\e[1;32m success\e[0m"
+else
+  echo -e "\e[1;33m failed\e[0m"
+  exit
+fi
 
 echo "Exract frontend content"
 cd /tmp
 unzip -o frontend.zip &>>$LOG_FILE
+if [ $? -eq 0 ]; then
+  echo -e "\e[1;32m success\e[0m"
+else
+  echo -e "\e[1;33m failed\e[0m"
+  exit
+fi
 
 echo "copy extracted content to Nginx path"
 cp -r  frontend-main/static/* usr/share/nginx/html &>>$LOG_FILE
+if [ $? -eq 0 ]; then
+  echo -e "\e[1;32m success\e[0m"
+else
+  echo -e "\e[1;33m failed\e[0m"
+  exit
+fi
 
 echo "copy Nginx roboshop config"
 cp frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+if [ $? -eq 0 ]; then
+  echo -e "\e[1;32m success\e[0m"
+else
+  echo -e "\e[1;33m failed\e[0m"
+  exit
+fi
 
 echo " start nginx"
 systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx &>>$LOG_FILE
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[1;32m success\e[0m"
+else
+  echo -e "\e[1;33m failed\e[0m"
+  exit
+fi
 
 
 
