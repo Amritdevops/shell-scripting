e4f674b32873cf9724dd36d175f721519613e452
amrit <paulamrit23878@gmail.com>
2022-02-09 17:28:57 -0500

    Updating shell-scripting

--- roboshop_project/roboshop.sh
+++ roboshop_project/roboshop.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
 ID= $(id -u)
 if [ $ID -ne 0 ]; then
   echo -e "\e[1;31m switch to root user\e[0m"
   exit
 fi
 
-bash components/$1.sh
+if [ -f components/$1.sh ]; then
+  bash components/$1.sh
+else
+  echo -e "\e[1;31m invalid input"
+fi
