747e4487584739868f2ad1b7c03e57569d8951bd
amrit <paulamrit23878@gmail.com>
2022-02-09 14:26:39 -0500

    Updating shell-scripting

--- roboshop_project/create-ec2.sh
+++ roboshop_project/create-ec2.sh
@@ -9,16 +9,17 @@ fi
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
 if [ -z "${AMI_ID}" ]; then
-  echo -e"\e[1;31mUnable to find the Image AMI_ID\e[0m"
+  echo -e "\e[1;31mUnable to find the Image AMI_ID\e[0m"
   exit
 else
-  echo "AMI_ID = ${AMI_ID}"
+  echo -e "\e[1;32mAMI ID = ${AMI_ID}\e[0m"
 fi
 
 PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].PrivateIpAddress' --output text)
+
 if [ -z "${PRIVATE_IP}" ]; then
-  aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
+  aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
 else
-  echo "Instance ${INSTANCE_NAME} is already exists. hence not creating"
+  echo "Instance ${INSTANCE_NAME} is already exists, hence not creating"
   exit
 fi
