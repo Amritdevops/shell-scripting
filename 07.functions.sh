65e237004dc6c61c23aab8ef740c2c4d9b43ae07
amrit <paulamrit23878@gmail.com>
2022-02-09 18:58:55 -0500

    Updating shell-scripting

---
+++ 07.functions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# aset of data to a name is called variable
+# set of command to a name is called as funtion
+#declare a function
+SAMPLE() {
+echo " welcome"
+}
+
+# call the function
+SAMPLE
